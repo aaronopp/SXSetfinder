90cf5ec6dc607863073eb617dfb6422a52b315dc
Aaron <aaronopp@gmail.com>
2018-03-13 17:57:22 -0500

    first version of matching alg almost done

--- app/test_boil.py
+++ app/test_boil.py
@@ -11,6 +11,10 @@ import webbrowser
 from server import block_until_token
 from utils import *
 
+import urllib, json
+
+url = "http://maps.googleapis.com/maps/api/geocode/json?address=google"
+
 user_artists = {0: 'Kendrick Lamar', 1: 'Kanye West', 2: 'John Mayer', 3: 'The Strokes', 4: 'Tame Impala', 5: 'The Undercover Dream Lovers', 6:'ZHU', 7:'Portugal. The Man', 
 8:'Jay Chou', 9:'Taylor Swift', 10:'Spoon'
 }
@@ -19,3 +23,39 @@ print type(user_artists)
 
 SXSW_artists = ['070 Shake', '16 the Olympus', '1982', '1kPhew', '1Playy' , "20 Somethin' Band/Tenth Child Inc. House Band", '24Hrs', '3 Hand Stephen', '5ive', '6Blocc', '90 One', 'A-RON', 'Aaron Lee Tasjan']
 
+url = "http://smarterplaylists.playlistmachinery.com/frog/path";
+
+start = time.time()
+lowest_path_length = 15
+for artist in SXSW_artists:
+    # print artist
+
+    f = { 'src' : 'Kendrick Lamar', 'dest' : artist}
+    # f = { 'src' : 'Kendrick Lamar', 'dest' : '1982'}
+    url_encoded = urllib.urlencode(f)
+    full_url = url + '?' + url_encoded
+    # print full_url
+    response = urllib.urlopen(full_url)
+    data = json.loads(response.read())
+    try:
+        raw_path = data['raw_path']
+    except KeyError as error:
+    	print 'no match from boilthefrog'
+    #print raw_path
+    #print type(raw_path)
+    path_length = len(raw_path)
+    if path_length < lowest_path_length:
+    	# print 'lower than max'
+    	# print f
+    	lowest_match = f
+    	# print path_length
+    	lowest_path_length = path_length
+
+print 'the lowest per your spotify top artist'
+print lowest_path_length
+print lowest_match
+lowest_matches_dict = {"spotify_artist": lowest_match['src'], "SXSW_artist": lowest_match['dest'], "path_length": lowest_path_length}
+print lowest_matches_dict
+#print data
+print 'took' 
+print time.time() - start
