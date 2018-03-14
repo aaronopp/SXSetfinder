f15cc65ed3300f6caf8f118a1664f23f9a3fbe64
Ranga <rangaraj.t@gmail.com>
2018-03-13 22:25:57 -0500

    groupme script

---
+++ app/groupme.py
@@ -0,0 +1,13 @@
+from groupy.client import Client
+import datetime
+
+def makeGroup(n, usrList):
+    client = Client.from_token('jI9arGAkDrtNWAhKdKLnjaj182R2L7RLV7q9odFj')
+    now=datetime.datetime.now()
+    nDate = n+now.strftime()
+    new_group = client.groups.create(name=nDate)
+    for i in usrList:
+        new_group.memberships.add(i[0], None, i[1], None)
+
+#testList=[['aaron', '8583533185'], ['ranga', '7329978242']]
+#makeGroup('test', testList)
